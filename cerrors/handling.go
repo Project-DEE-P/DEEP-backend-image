68fef860d05405b8e9ddd68750049926b22c2dfa
JJerBum <2hanjunbum6@gmail.com>
2023-11-04 15:46:03 +0900

    feat: errorhandling

---
+++ cerrors/handling.go
@@ -0,0 +1,46 @@
+package cerrors
+
+import (
+	"encoding/json"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// GeneralResponse 구조체는 3중 참조를 막기 위한 구조체 입니다.
+type generalResponse struct {
+	Status  uint   `json:"status"`
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+}
+
+func NewErrorHandler() func(*fiber.Ctx, error) error {
+	return func(c *fiber.Ctx, err error) error {
+
+		if _, isValidationError := err.(validationErr); isValidationError {
+			data := err.Error()
+			var messages []map[string]interface{}
+
+			errJson := json.Unmarshal([]byte(data), &messages)
+			Sniff(errJson)
+			return c.Status(fiber.StatusBadRequest).JSON(generalResponse{
+				Status:  fiber.StatusBadRequest,
+				Message: "Bad Request",
+				Data:    messages,
+			})
+		}
+
+		if _, isParsingErr := err.(parsingErr); isParsingErr {
+			c.Status(fiber.StatusBadRequest).JSON(generalResponse{
+				Status:  fiber.StatusBadRequest,
+				Message: "Parsing error",
+				Data:    err.Error(),
+			})
+		}
+
+		return c.Status(fiber.StatusInternalServerError).JSON(generalResponse{
+			Status:  fiber.StatusInternalServerError,
+			Message: "General Error",
+			Data:    err.Error(),
+		})
+	}
+}
