22ebf04204bcc085a540f86ff071cd0bd55047fa
JJerBum <2hanjunbum6@gmail.com>
2023-11-27 21:47:41 +0900

    바꿈

--- middleware/authenticate.go
+++ middleware/authenticate.go
@@ -13,7 +13,7 @@ func Authenticate(c *fiber.Ctx) error {
 
 	if token == "" {
 		cerrors.AuthorizationErr("당신은 ACCESS-KEY를 소지하고 않고 있습니다.")
-	} else if token != os.Getenv("ACCESS-KEY") {
+	} else if token != os.Getenv("ACCESS_KEY") {
 		cerrors.AuthorizationErr("당신은 유효하지 못한 ACCESS-KEY를 소지하고 있습니다.")
 	}
 
